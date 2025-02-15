f4a5c05935f5afc73a342d0d898dd658ba4b0f7b
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-02-15 17:06:54 +0100

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ExportController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/category/{categoryId}', [ProductController::class, 'byCategory']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+Route::get('/products/category/{categoryId}/export', [ExportController::class, 'exportCategoryProducts']);
